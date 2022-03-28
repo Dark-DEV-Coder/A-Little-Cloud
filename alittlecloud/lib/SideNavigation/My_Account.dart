c42f1092e4ca49c920e13410e35a992899e33103
luanym1234vip <camduyen20012@gmail.com>
2022-03-28 10:34:57 +0800

    Updatecode 5

--- alittlecloud/lib/SideNavigation/My_Account.dart
+++ alittlecloud/lib/SideNavigation/My_Account.dart
@@ -1,24 +1,72 @@
 import 'package:alittlecloud/Custom_NavBar/custom_app_bar.dart';
 import 'package:flutter/material.dart';
+
 class Account extends StatelessWidget {
-  const Account ({Key? key}) : super(key: key);
+  const Account({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) => Scaffold(
-    body: SafeArea(
-      top: false,
-      child: Column(
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.only(
-                top: MediaQuery.of(context).padding.top,),
-            child: Image.network('https://i.pinimg.com/564x/cf/f0/59/cff05928c82eb0d047411c3b70e11235.jpg',
-              height: 146,
-            ),
-          ),
+        body: Container(
+          padding: EdgeInsets.only(left: 16, top: 25, right: 16),
+          child: ListView(
+            children: [
+              Text(
+                "Edit Profile",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Center(
+                child: Stack(
+                  children: [
+                    Container(
+                        width: 130,
+                        height: 130,
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                                width: 4,
+                                color:
+                                    Theme.of(context).scaffoldBackgroundColor),
+                            boxShadow: [
+                              BoxShadow(
+                                  spreadRadius: 2,
+                                  blurRadius: 10,
+                                  color: Colors.black.withOpacity(0.1),
+                                  offset: Offset(0, 10))
+                            ],
+                            shape: BoxShape.circle,
+                            image: DecorationImage(
+                                fit: BoxFit.cover,
+                                image: NetworkImage(
+                                  "https://i.pinimg.com/564x/cf/f0/59/cff05928c82eb0d047411c3b70e11235.jpg",
+                                )))),
+                    Positioned(
+                        bottom: 0,
+                        right: 0,
+                        child: Container(
+                          height: 40,
+                          width: 40,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            border: Border.all(
+                              width: 4,
+                              color: Theme.of(context).scaffoldBackgroundColor,
+                            ),
+                            color: Colors.green,
+                          ),
+                          child: Icon(
+                            Icons.edit,
+                            color: Colors.white,
+                          ),
+                        )),
+                  ],
 
-        ],
-      ),
-    ),
-    );
+                ),
+
+              ),
+            ],
+          ),
+        ),
+      );
 }
